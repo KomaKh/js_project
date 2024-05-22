53fcdaf2dd84261973f78cb449f8e5e6cc222e61
kamron <khaytkulovkamron@gmail.com>
2024-05-22 10:12:45 +0500

    add if statement

--- js/script.js
+++ js/script.js
@@ -11,8 +11,24 @@ const personalMovieDb = {
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = prompt('На сколько оцените его?', '');
-    
-    personalMovieDb.movies[a] = b;
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+       personalMovieDb.movies[a] = b;
+       console.log('Done!');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+if (personalMovieDb.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+    console.log("Вы класический зритель");
+} else if (personalMovieDb.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
 
 console.log(personalMovieDb);
